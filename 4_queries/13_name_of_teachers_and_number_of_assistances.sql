eb3d69957d8beab7d5d3159fd1ac4fd51eb87c18
Selin Huang <selin.hyx@gmail.com>
2019-07-23 16:43:04 +0000

    completed the 4th round of queries

---
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,21 @@
+SELECT 
+  cohorts.name as cohort,
+  teachers.name as teacher,
+  count(*) as total_assistances
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+JOIN teachers ON teachers.id = teacher_id
+GROUP BY teachers.name, cohorts.name
+HAVING cohorts.name = 'JUL02'
+ORDER BY teacher;
+
+
+-- SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+-- FROM teachers
+-- JOIN assistance_requests ON teacher_id = teachers.id
+-- JOIN students ON student_id = students.id
+-- JOIN cohorts ON cohort_id = cohorts.id
+-- WHERE cohorts.name = 'JUL02'
+-- GROUP BY teachers.name, cohorts.name
+-- ORDER BY teacher;
